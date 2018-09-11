b5965823bc51bf4e896e9e1a37f9200d99616636
Michael Heinsohn <mheinsohn@google.com>
2018-09-11 14:16:50 -0700

    Added variables used in new routines.

--- globals.h
+++ globals.h
@@ -1,9 +1,13 @@
 #pragma once
 
-// Global constants for the program. The numbers are arbitrary, except for boardsize.
+// Global constants for the program. The numbers are arbitrary.
 const int BACKTHREE = 100;
-const int BOARDSIZE = 40;
 const int NOMOVE = 200;
 const int NOTFOUND = 300;
 const int RAILROAD = 400;
 const int UTILITY = 500;
+
+// These numbers are specific, not arbitrary.
+const int BOARDSIZE = 40;
+const int JAIL = 10;
+const int GOTOJAIL = 30;
