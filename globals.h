7baacd869cb22c4a614015630795412a11b1bee2
Michael Heinsohn <mheinsohn@google.com>
2018-09-25 11:25:11 -0700

    Update globals.h
    Added all destinations. This is not a list of every slot on the board, just the ones that have move destinations, for example sent there by a Chance card.

--- globals.h
+++ globals.h
@@ -1,6 +1,7 @@
 #pragma once
 
-// Global constants for the program. The numbers are arbitrary.
+// Global constants for the program. The numbers are arbitrary, but must be higher than 40 
+// to prevent collisions with board slots.
 const int BACKTHREE = 100;
 const int NOMOVE = 200;
 const int NOTFOUND = 300;
@@ -9,5 +10,21 @@ const int UTILITY = 500;
 
 // These numbers are specific, not arbitrary.
 const int BOARDSIZE = 40;
+const int GO = 0;
+const int COMMUNITYCHEST1 = 2;
+const int READING_RAILROAD = 5;
+const int CHANCE1 = 7;
 const int JAIL = 10;
+const int ST_CHARLES_PLACE = 11;
+const int ELECTRIC_COMPANY = 12;
+const int PENNSYLVANIA_RAILROAD = 15;
+const int COMMUNITYCHEST2 = 17;
+const int CHANCE2 = 22;
+const int ILLINOIS_AVENUE = 24;
+const int BO_RAILROAD = 25;
+const int WATER_WORKS = 28;
 const int GOTOJAIL = 30;
+const int COMMUNITYCHEST3 = 33;
+const int SHORTLINE_RAILROAD = 35;
+const int CHANCE3 = 36;
+const int BOARDWALK = 39;
